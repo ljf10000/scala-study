a49679cec53d2cc0f612ce82a180f5c1b28c4f9a
root <root@host06.(none)>
2017-08-14 10:59:02 +0800

    .

--- jkafka/submit.sh
+++ jkafka/submit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+readonly SPARK_EXT=${SPARK_HOME}/extrajars
 readonly JAR_SPARK_STREAMING_KAFKA='spark-streaming-kafka-0-8-assembly_2.11-2.1.1.jar'
 readonly JAR_TARGET='target/scala-2.11/jkafka_2.11-1.0.jar'
+readonly JAR_JXDR='../jxdr/target/scala-2.11/jxdr_2.11-1.0.jar'
 readonly TOPICS='xdr,xdrFile,xdrHttp'
 readonly BROKERS="192.168.1.103:9092\
 ,192.168.1.105:9092\
@@ -11,13 +13,26 @@ readonly BROKERS="192.168.1.103:9092\
 
 readonly PARQUET_PATH='/test/parquet/jkafka'
 
-spark-submit \
-	--master yarn \
-	--deploy-mode cluster \
-	--driver-memory 1g \
-	--executor-memory 1g \
-	--num-executors 5 \
-	--executor-cores 4 \
+echo "TOPICS=${TOPICS}"
+echo "BROKERS=${BROKERS}"
+
+declare opt
+mode=$1; mode=${mode:-cluster}
+
+case ${mode} in
+cluster)
+	opt='--master yarn --deploy-mode cluster'
+	;;
+local|*)
+	opt='--master local'
+	;;
+esac
+
+spark-submit ${opt} \
+	--driver-memory 512m \
+	--executor-memory 512m \
+	--num-executors 1 \
+	--executor-cores 3 \
 	--conf spark.default.parallelism=60 \
 	--conf spark.storage.memoryFraction=0.4 \
 	--conf spark.shuffle.memoryFraction=0.4 \
@@ -26,8 +41,9 @@ spark-submit \
 	--conf spark.streaming.kafka.maxRatePerPartition=1000 \
 	--conf spark.cleaner.referenceTracking.blocking=true \
 	--conf spark.streaming.concurrentJobs=16 \
-	--jars ${SPARK_HOME}/extrajars/${JAR_SPARK_STREAMING_KAFKA} \
-	--class "jkafka" \
+	--jars ${SPARK_EXT}/${JAR_SPARK_STREAMING_KAFKA} \
+	--jars ${JAR_JXDR} \
+	--class "jkafka.Jkafka" \
 	${JAR_TARGET} \
 		${BROKERS} \
 		${TOPICS} \
